9e21c2ba0431987264b4285ee2237fdb745c7dbd
Jeff Chong <jeffchong5@gmail.com>
2021-12-19 23:23:50 -0800

    Implemented user input and calculates percent error

--- src/EulerSimulation.java
+++ src/EulerSimulation.java
@@ -3,7 +3,10 @@ import java.awt.geom.Ellipse2D;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtils;
 import org.jfree.chart.JFreeChart;
@@ -16,14 +19,40 @@ import org.jfree.data.xy.XYSeriesCollection;
 public class EulerSimulation {
     public static void main(String[] args) throws IOException {
 
+        Scanner consoleScanner = new Scanner(System.in);
+        int numSimulations = 1000000;
+        boolean validInput = false;
+        double estimatedValue = 0;
+
+        System.out.println("This program estimates Euler's Number given a specified number of simulations used to" +
+                " generate the estimate.");
+
+        do{
+            try{
+                System.out.print("Enter the number of simulations: ");
+                numSimulations = consoleScanner.nextInt();
+                validInput = true;
+                if(numSimulations <= 0) {
+                    validInput = false;
+                    System.out.println("Please enter a positive integer.");
+                }
+            }catch (InputMismatchException e)
+            {
+                consoleScanner.nextLine();
+                validInput = false;
+                System.out.println("Please enter an integer.");
+            }
+        }while(!validInput);
+
+        System.out.println(numSimulations);
+
+
 
         XYSeriesCollection dataset = new XYSeriesCollection();
         XYSeries series1 = new XYSeries("Avg. number of summations");
-
-
         List<Integer> values = new ArrayList<>();
 
-        int numSimulations = 1000000;
+
         double countSum = 0;
 
         for(int i = 0; i < numSimulations; i++) {
@@ -39,7 +68,8 @@ public class EulerSimulation {
                 }
             }
             //series1.add(i+1,values.stream().mapToInt(e -> e).average().getAsDouble());
-            series1.add(i+1, countSum/(i+1));
+            estimatedValue = countSum/(i+1);
+            series1.add(i+1, estimatedValue);
         }
         dataset.addSeries(series1);
 
@@ -49,8 +79,12 @@ public class EulerSimulation {
                 "Average summations", // Y-Axis Label
                 dataset // Dataset for the Chart
         );
+        
+        System.out.println("Estimated value of e: " + estimatedValue);
+        System.out.println("Percent error between estimated value and theoretical value of Euler's Number: " +
+                Math.abs(estimatedValue - Math.exp(1)) * 100 + "%.");
 
-
+        System.out.println("Please do not close the program while the graph is being generated.");
 
         XYPlot plot = (XYPlot) scatterPlot.getPlot();
 
@@ -73,6 +107,9 @@ public class EulerSimulation {
 
         String fileName = "Simulation of Euler's Number with " + numSimulations + " Simulations.png";
         ChartUtils.saveChartAsPNG(new File(fileName), scatterPlot, 1000, 1000);
+
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("Graph of Euler's Number simulation is complete.");
 
     }
 }
